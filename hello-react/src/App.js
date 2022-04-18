ccf03098aa06985271fe557364e5a46e78dddf85
johj703 <johj703@nate.com>
2022-04-18 21:45:15 +0900

    undefined를 렌더링하지 않기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,8 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name = '리액트';
-  return <div>{name === '리액트' && <h1>리액트 입니다.</h1>}</div>;
+  const name = undefined;
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
