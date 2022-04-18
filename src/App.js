a1fa94cbdaabffe5c137f795c56530a1556105fa
johj703 <johj703@nate.com>
2022-04-18 18:32:54 +0900

    undefined를 렌더링하지 않기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   // const = 한번 지정하고 나면 변경이 불가능한 상수를 선언할 때 사용
   // let = 동적인 값을 담을 수 있는 변수를 선언할 때 사용
   // let, const는 scope가 함수 단위가 아니라 블록 단위다.
-  const name = '리액트';
-  return (
-    <div>
-      {name === '리액트' && 
-        <h1>리액트 입니다.</h1>
-      }
-    </div>
-  );
+  const name = undefined;
+  return <div>{name || '리액트'}</div>
 }
 
 export default App;
