53e863ee9e8f8f47f17cee6dd5432b3e9c76517c
johj703 <johj703@nate.com>
2022-04-18 18:38:36 +0900

    인라인 스타일링

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ function App() {
   // const = 한번 지정하고 나면 변경이 불가능한 상수를 선언할 때 사용
   // let = 동적인 값을 담을 수 있는 변수를 선언할 때 사용
   // let, const는 scope가 함수 단위가 아니라 블록 단위다.
-  const name = undefined;
-  return <div>{name || '리액트'}</div>
+  const name = '리액트';
+  const style = {
+  // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기볍으로 작성된다.
+  backgroundColor: 'black',
+  color: 'aqua',
+  fontSize: '48px',
+  fontWeight: 'bold',
+  padding: 16
+  };
+  return <div style={style}>{name}</div>
 }
 
 export default App;
