2a90dcea83b0755740ef2eeb8442ac24b7352c6e
johj703 <johj703@nate.com>
2022-04-18 19:35:41 +0900

    2장 마지막 주석

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ function App() {
   // let = 동적인 값을 담을 수 있는 변수를 선언할 때 사용
   // let, const는 scope가 함수 단위가 아니라 블록 단위다.
   const name = '리액트';
-  const style = {
-  // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기볍으로 작성된다.
-  backgroundColor: 'black',
-  color: 'aqua',
-  fontSize: '48px',
-  fontWeight: 'bold',
-  padding: 16
-  };
-  return <div style={style}>{name}</div>
+  return (
+    <>
+      {/* 주석은 이렇게 작성합니다. */}
+      <div
+        className="react" // 시작 태그를 여러 줄로 작성하게 된다면 여기에 주석을 작성할 수 있다.
+      >
+        {name}
+      </div>
+      // 하지만 이런 주석이나
+      /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
+      <input />
+    </>
+  );
 }
 
 export default App;
