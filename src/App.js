f556fa05cb45455a3231e313d09aeeece004d2e4
johj703 <johj703@nate.com>
2022-04-18 18:18:57 +0900

    자바스크립트 표현

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const = 한번 지정하고 나면 변경이 불가능한 상수를 선언할 때 사용
+  // let = 동적인 값을 담을 수 있는 변수를 선언할 때 사용
+  // let, const는 scope가 함수 단위가 아니라 블록 단위다.
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name} 안녕!</h1>
+      <h2>잘 작동하니?</h2>
+    </>
   );
 }
 
