1d9d6a3f19bf3553610e2aee4aaa2dcbf94a325f
johj703 <johj703@nate.com>
2022-04-21 18:26:41 +0900

    비구조화 할당 문법을 통해 props 내부 값 추출하기

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,13 +1,14 @@
-const MyComponent = props => {
+const MyComponent = ({ name, children }) => {
     return (
         <div>
-        안녕하세요. 제 이름은 {props.name}입니다. <br />
-        children 값은 {props.children}
+        안녕하세요. 제 이름은 {name}입니다. <br />
+        children 값은 {children}
         입니다.
         </div>
     );
 };
-
+// 비구조화 할당 = 구조 분해 문법이라고도 불리며, 함수의 파라미터 부분에서도 사용할 수 있다.
+// 만약 함수의 파라미터가 객체라면 그 값을 바로 비구조화해서 사용하는 것이다.
 MyComponent.defaultProps = {
     name: '기본 이름'
 };
