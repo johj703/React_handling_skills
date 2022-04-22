be657ae1e6499c0163cc0fe1229e44f442a11ef6
johj703 <johj703@nate.com>
2022-04-22 21:24:32 +0900

    state 객체 안에 여러 값이 있을 때

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -7,14 +7,16 @@ class Counter extends Component {
         super(props);
         // state의 초기값 설정하기
         this.state = {
-            number: 0
+            number: 0,
+            fixedNumber: 0
         };
     }
     render(){
-        const { number } = this.state; // state를 조회할 때는 this.state로 조회한다.
+        const { number, fixedNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
         return (
             <div>
                 <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
                 <button
                     // onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정한다!
                     // 이벤트로 설정할 함수를 넣어 줄 때는 화살표 함수 문법을 사용하여 넣어 주어야 한다.
