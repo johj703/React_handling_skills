54cb8494ef74dd5c6ce336ffd43f0af5b8102c9c
johj703 <johj703@nate.com>
2022-04-23 12:39:19 +0900

    임의 메서드 만들기

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -6,6 +6,25 @@ class EventPractice extends Component {
         message: ''
     }
 
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
     render(){
         return (
             <div>
@@ -15,25 +34,9 @@ class EventPractice extends Component {
                     name="message"
                     placeholder="아무거나 입력해 보세요."
                     value={this.state.message}
-                    onChange={
-                        // e 객체는 console에서 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체다.
-                        // 네이티브 이벤트와 인터페이스가 같으므로 순수 자바스크립트에서 HTML 이벤트를 다룰 때와 똑같이 사용하면 된다!
-                        // onChange 이벤트가 발생할 때, console.log에 e.target.value를 입력하면 값이 바뀔 때마다 콘솔에 기록하게 된다!
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            })
-                        }
-                    }
+                    onChange={this.handleClick}
                 />
-                <button onClick={
-                    () => {
-                        alert(this.state.message);
-                        this.setState({
-                            message: ''
-                        });
-                    }
-                }>확인</button>
+                <button onClick={this.handleClick}>확인</button>
             </div>
         );
     }
