e8f4535a382ba595040d085bae8ba86e0be27d06
johj703 <johj703@nate.com>
2022-04-23 12:11:01 +0900

    onChange 이벤트 설정

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -5,6 +5,19 @@ class EventPractice extends Component {
         return (
             <div>
                 <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해 보세요."
+                    onChange={
+                        // e 객체는 console에서 SyntheticEvent로 웹 브라우저의 네이티브 이벤트를 감싸는 객체다.
+                        // 네이티브 이벤트와 인터페이스가 같으므로 순수 자바스크립트에서 HTML 이벤트를 다룰 때와 똑같이 사용하면 된다!
+                        // onChange 이벤트가 발생할 때, console.log에 e.target.value를 입력하면 값이 바뀔 때마다 콘솔에 기록하게 된다!
+                        (e) => {
+                            console.log(e.target.value);
+                        }
+                    }
+                />
             </div>
         );
     }
