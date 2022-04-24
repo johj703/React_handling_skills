097326d06df6852a0aa222131b19ed5c1658f720
johj703 <johj703@nate.com>
2022-04-24 20:51:39 +0900

    컴포넌트에 ref 달기 초기 설정

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,10 @@
 import { Component } from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample />
+      <ScrollBox />
     );
   }
 }
