b8d47c27b4e5b88d4277383f70ad3be576c5efc3
johj703 <johj703@nate.com>
2022-04-25 12:43:39 +0900

    key 사용

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,7 +1,8 @@
 const IterationSample = () => {
     const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map(name => <li>{name}</li>);
-    return <ul>{nameList}</ul>;
+    // key = 컴포넌트 배열을 렌더링했을 때, 어떤 원소에 변동이 있었는지 알아내려고 사용하는 것이다.
+    const namesList = names.map((name, index) => <li key={index}>{name}</li>);
+    return <ul>{namesList}</ul>;
 };
 
 export default IterationSample;
