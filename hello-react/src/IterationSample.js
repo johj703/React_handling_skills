81c10c7424ea787e0d682e244c86744b3e74cf01
johj703 <johj703@nate.com>
2022-04-25 13:11:10 +0900

    key 응용 데이터 추가 기능 구현하기 onClick 함수 이벤트 추가

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -11,12 +11,24 @@ const IterationSample = () => {
     const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
 
     const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        // 배열에 새 항목을 추가할 때
+        // push 함수 = 기존 배열 자체를 변경할 때 사용
+        // concat 함수 = 새로운 배열을 만들어 줄 때 사용
+        const nextNames = names.concat({
+            id: nextId, //nestId 값을 id로 설정하고
+            text: inputText
+        });
+        setNextId(nextId + 1); //nextId 값에 1을 더해 준다.
+        setNames(nextNames); // names 값을 업데이트 한다.
+        setInputText(''); // inputText를 비운다.
+    };
     
     const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
     return (
         <>
             <input value={inputText} onChange={onChange} />
-            <button>추가</button>
+            <button onClick={onClick}>추가</button>
             <ul>{namesList}</ul>;
         </>
     );
