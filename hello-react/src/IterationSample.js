db7b242eb695c179c48b44decfce29b95bb3517d
johj703 <johj703@nate.com>
2022-04-25 12:53:29 +0900

    key 응용 초기 상태 설정

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    // key = 컴포넌트 배열을 렌더링했을 때, 어떤 원소에 변동이 있었는지 알아내려고 사용하는 것이다.
-    const namesList = names.map((name, index) => <li key={index}>{name}</li>);
+    const [names, setNames] = useState([
+        { id: 1, text: '눈사람'},
+        { id: 2, text: '얼음'},
+        { id: 3, text: '눈'},
+        { id: 4, text: '바람'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+    
+    const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
     return <ul>{namesList}</ul>;
 };
 
