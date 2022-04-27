2c7fb0554a14bfd0767b037e5f154041d8ca7456
johj703 <johj703@nate.com>
2022-04-27 12:56:31 +0900

    useState 만들기

--- hooks-tutorial/src/Counter.js
+++ hooks-tutorial/src/Counter.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 
 const Counter = () => {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState(0);
 
     return (
         <div>
             <p>
-                현재 카운터 값은 <b>{value}</b>
+                현재 카운터 값은 <b>{value}</b>입니다.
             </p>
             <button onClick={() => setValue(value + 1)}>+1</button>
             <button onClick={() => setValue(value - 1)}>-1</button>
