d44279f91855fd6739b7cff750db15475c26388f
johj703 <johj703@nate.com>
2022-04-28 16:43:51 +0900

    useEffect 적용

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
-    
+    // useEffect = 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 수행하도록 설정할 수 있는 Hook이다.
+    useEffect(() => {
+        console.log('렌더링이 완료되었습니다!')
+        console.log({
+            name,
+            nickname
+        });
+    });
+
     const onChangeName = e => {
         setName(e.target.value);
     };
