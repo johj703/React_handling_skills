525747477c89d83c30e3f5a4a2c30fd400362a6e
johj703 <johj703@nate.com>
2022-04-29 15:07:01 +0900

    useRef 등록 버튼을 눌렀을 대 포커스가 input쪽으로 넘어가도록 하는 기능

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useMemo, useCallback, useRef } from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중...');
@@ -10,6 +10,7 @@ const getAverage = numbers => {
 const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
+    const inputEl = useRef(null);
 
     const onChange = useCallback(e => {
         setNumber(e.target.value);
@@ -18,13 +19,14 @@ const Average = () => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    }, [number, list]);
+        inputEl.current.focus();
+    }, [number, list]); // number 혹은 list가 바뀌었을 때만 함수 생성
     // useMemo = 렌더링하는 과정에서 특정 값이 바뀌었을 때만 연선을 실행하고, 원하는 값이 바뀌지 않았다면 이전에 연산했던 결과를 다시 사용하는 방법
     const avg = useMemo(() => getAverage(list), [list]);
 
     return(
         <div>
-            <input value={number} onChange={onChange} />
+            <input value={number} onChange={onChange} ref={inputEl} />
             <button onClick={onInsert}>등록</button>
             <ul>
                 {list.map((value, index) => (
