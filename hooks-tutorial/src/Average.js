e5f4e3fc24123437528078dbe2b14b59a3735b20
johj703 <johj703@nate.com>
2022-04-29 14:59:33 +0900

    useCallback 렌더링 성능을 최적화해야하는 상황에서 사용, 만들어놨던 함수를 재사용할 수 있다.

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useCallback } from 'react';
 
 const getAverage = numbers => {
     console.log('평균값 계산 중...');
@@ -11,14 +11,14 @@ const Average = () => {
     const [list, setList] = useState([]);
     const [number, setNumber] = useState('');
 
-    const onChange = e => {
+    const onChange = useCallback(e => {
         setNumber(e.target.value);
-    };
-    const onInsert = e => {
+    }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+    const onInsert = useCallback(() => {
         const nextList = list.concat(parseInt(number));
         setList(nextList);
         setNumber('');
-    };
+    }, [number, list]);
     // useMemo = 렌더링하는 과정에서 특정 값이 바뀌었을 때만 연선을 실행하고, 원하는 값이 바뀌지 않았다면 이전에 연산했던 결과를 다시 사용하는 방법
     const avg = useMemo(() => getAverage(list), [list]);
 
