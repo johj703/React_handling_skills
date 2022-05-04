8e80cad01d859c3c709b7f28758db45a11d64e52
johj703 <johj703@nate.com>
2022-05-04 12:00:39 +0900

    cover 클래스 추가

--- todo-app/src/component/TodoListItem.js
+++ todo-app/src/component/TodoListItem.js
@@ -8,11 +8,13 @@ import './TodoListItem.scss';
 const TodoListItem = () => {
     return (
         <div className="TodoListItem">
-            <div className="MdCheckBox">
-                <MdCheckBoxOutlineBlank />
-            </div>
-            <div>
-                <div className="text">할 일</div>
+            <div className="cover">
+                <div className="MdCheckBox">
+                    <MdCheckBoxOutlineBlank />
+                </div>
+                <div>
+                    <div className="text">할 일</div>
+                </div>
             </div>
             <div className="remove">
                 <MdRemoveCircleOutline />
