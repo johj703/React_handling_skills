42198a653db2059af5512940687fc677e91719a3
johj703 <johj703@nate.com>
2022-05-09 20:45:50 +0900

    커밋 메세지 수정

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+<<<<<<< HEAD
         <li>
           <Link to="/profile/velopert">velopert 프로필</Link>
         </li>
@@ -24,6 +25,12 @@ const App = () => {
       <Route path="/" component={Home} exact={true}/>
       <Route path={['/about', '/info']} component={About} />
       <Route path="/profile/:username" component={Profile} />
+=======
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true}/>
+      <Route path="/About" component={About} />
+>>>>>>> parent of 56a261c0 (리액트 라우터로 SPA 개발하기 - Routo 하나에 여러 개의 path 설정하기)
     </div>
   );
 };
