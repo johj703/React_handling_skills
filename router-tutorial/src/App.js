73be5dfcf061e9cea77347a4e547b83451f30fd8
johj703 <johj703@nate.com>
2022-05-10 02:26:17 +0900

    서브 라우트

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 const App = () => {
   return (
@@ -13,24 +13,14 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
-<<<<<<< HEAD
         <li>
-          <Link to="/profile/velopert">velopert 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">gildong 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true}/>
+      <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
-=======
-      </ul>
-      <hr />
-      <Route path="/" component={Home} exact={true}/>
-      <Route path="/About" component={About} />
->>>>>>> parent of 56a261c0 (리액트 라우터로 SPA 개발하기 - Routo 하나에 여러 개의 path 설정하기)
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 };
