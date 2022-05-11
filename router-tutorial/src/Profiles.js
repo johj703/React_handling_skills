62b8dd8b11af021b5e75a0e7475cb3edd405d628
johj703 <johj703@nate.com>
2022-05-11 22:09:57 +0900

    리액트 라우터로 SPA 개발하기 - 리액트 라우터 부가 기능 - NavLink

--- router-tutorial/src/Profiles.js
+++ router-tutorial/src/Profiles.js
@@ -1,24 +1,34 @@
-import { Link, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Profile from './Profile';
 import WithRouterSample from './WithRouterSample';
 
+// NavLink는 Link와 비숫하다. 현재 경로와 Link에서 사용하는 경로가 일치하는 경우 특정 스타일 혹은 CSS 클래스를 적용할 수 있는 컴포넌트다.
+// NavLink에서 링크가 활성화되었을 때의 스타일을 적용할 때는 activeStyle 값을, CSS 클래스를 적용할 때는 activeClassName 값을 props로 넣어주면 된다.
 const Profiles = () => {
-    return(
+    const activeStyle = {
+        background: 'black',
+        color: 'white'
+    };
+    return (
         <div>
             <h3>사용자 목록:</h3>
             <ul>
                 <li>
-                    <Link to="/profiles/velopert">velopert</Link>
+                    <NavLink activeStyle={activeStyle} to="/profiles/velopert">
+                        velopert
+                    </NavLink>
                 </li>
                 <li>
-                    <Link to="/profiles/gildong">gildong</Link>
+                    <NavLink activeStyle={activeStyle} to="/profiles/gildong">
+                        gildong
+                    </NavLink>
                 </li>
             </ul>
 
-            <Route 
-             path="/profiles"
-             exact
-             render={() => <div>사용자를 선택해 주세요.</div>}
+            <Route
+                path="/profiles"
+                exact
+                render={() => <div>사용자를 선택해 주세요.</div>}
             />
             <Route path="/profiles/:username" component={Profile} />
             <WithRouterSample />
