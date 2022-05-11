8ff1d9d908b9757e4b245fa4ae80594b23658e5d
johj703 <johj703@nate.com>
2022-05-11 22:01:44 +0900

    리액트 라우터로 SPA 개발하기 - 리액트 라우터 부가 기능 - Switch

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,9 +1,10 @@
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
 import HistorySample from './HistorySample';
-
+// Switch 컴포넌트는 여러 Route를 감싸서 그 중 일치하는 단 하나의 라우트만을 렌더링시켜 준다.
+// Switch를 사용하면 모든 규칙과 일치하지 않을 때 보여 줄 Not Found 페이지도 구현할 수 있다.
 const App = () => {
   return (
     <div>
@@ -22,10 +23,21 @@ const App = () => {
         </li>
       </ul>
       <hr />
+      <Switch>
         <Route path="/" component={Home} exact={true} />
         <Route path={['/about', '/info']} component={About} />
         <Route path="/profiles" component={Profiles} />
         <Route path="/history" component={HistorySample} />
+        <Route 
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링이 된다.
+          render={({ location }) => (
+           <div>
+             <h2>이 페이지는 존재하지 않습니다:</h2>
+             <p>{location.pathname}</p>
+           </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
