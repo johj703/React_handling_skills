ad1a69af876f57b45f62293caf611b257ff0b207
johj703 <johj703@nate.com>
2022-05-13 23:22:11 +0900

    외부 API를 연동하여 뉴스 뷰어 만들기 - 데이터 연동하기

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -1,5 +1,7 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -22,14 +24,41 @@ const sampleArticle = {
 };
 
 const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        // async를 사용하는 함수를 따로 선언
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await axios.get(
+                    'https://newsapi.org/v2/top-headlines?contry=kr&apikey=2863d495030043e9b67b28b25340bebf',
+                );
+                setArticles(response.data.articles);
+            } catch (e) {
+                console.log(e);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    // 대기 중일 때
+    if (loading) {
+        return <NewsListBlock>대기 중...</NewsListBlock>
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+        return null;
+    }
+
+    // articles 값이 유효할 때
     return(
         <NewsListBlock>
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
-            <NewsItem article={sampleArticle} />
+            {articles.map(article => (
+            <NewsItem key={article.url} article={sampleArticle} />
+            ))}
         </NewsListBlock>
     );
 };
