5a62615854b291ed16b8dd0563bdda851b5fca70
johj703 <johj703@nate.com>
2022-05-15 19:51:43 +0900

    외부 API를 연동하여 뉴스 뷰어 만들기 - 카테고리 기능 구현하기 - 카테고리 선택 UI 만들기(백틱으로 수정), API를 호출할 때 카테고리 지정하기)

--- news-viewer/src/components/NewsList.js
+++ news-viewer/src/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
     const [articles, setArticles] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -25,8 +25,9 @@ const NewsList = () => {
         const fetchData = async () => {
             setLoading(true);
             try {
+                const query = category === 'all' ? '' : `&category=${category}`;
                 const response = await axios.get(
-                    'https://newsapi.org/v2/top-headlines?country=kr&apikey=2863d495030043e9b67b28b25340bebf',
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=2863d495030043e9b67b28b25340bebf`,
                 );
                 setArticles(response.data.articles);
             } catch (e) {
@@ -35,7 +36,7 @@ const NewsList = () => {
             setLoading(false);
         };
         fetchData();
-    }, []);
+    }, [category]);
 
     // 대기 중일 때
     if (loading) {
