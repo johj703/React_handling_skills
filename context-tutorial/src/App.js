e62cc2dbcb6b54cef5067b996ff1694ecf5abd2d
johj703 <johj703@nate.com>
2022-05-18 21:15:24 +0900

    Context API - Context API 사용법 익히기 - Provider(올바른 사용법)

--- context-tutorial/src/App.js
+++ context-tutorial/src/App.js
@@ -1,10 +1,16 @@
 import ColorBox from "./components/ColorBox";
+import ColorContext from './contexts/color';
 
+// 기존 createContext 함수를 사용할 때는 파라미터로 Context의 기본값을 넣어 주었지만,
+// 이 기본 값은 Provider를 사용하지 않았을 때만 사용된다. 만약 Provider는 사용했는데 value를
+// 명시하지 않았다면, 이 기본값을 사용하지 않기 때문에 오류가 발생한다.
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: 'red' }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
