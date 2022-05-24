28bffdcff015fc11e679ef582e638f8805850231
johj703 <johj703@nate.com>
2022-05-24 21:05:19 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 - 리덕스 관련 코드 작성하기 - counter 모듈 작성하기 - 액션 생성 함수 만들기

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,3 +1,6 @@
 // 액션 타입 정의 : 액션 타입은 대문자로 정의하고, 문자열 내용은 '모듈 이름/액션 이름'과 같은 형태로 작성한다.
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+const INCREASE = 'Counter/INCREASE';
+const DECREASE = 'Counter/DECREASE';
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
