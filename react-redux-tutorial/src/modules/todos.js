82e67ec83cea67f4c7e468c0cda0ee6da013c9f3
johj703 <johj703@nate.com>
2022-05-24 21:49:11 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 - 리덕스 관련 코드 작성하기 - todos 모듈 만들기 - 액션 타입 정의하기

---
+++ react-redux-tutorial/src/modules/todos.js
@@ -0,0 +1,4 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경한다.
+const INSERT = 'dotos/INSERT'; // 새로운 todos를 등록한다.
+const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제한다.
+const REMOVE = 'todos/REMOVE'; // todo를 제거한다.
