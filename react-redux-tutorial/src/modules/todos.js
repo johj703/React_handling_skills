5bcb9d7d175cd90312e25415cccae02e6cc1b385
johj703 <johj703@nate.com>
2022-05-25 19:54:24 +0900

    오타 수정

--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,9 +1,9 @@
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경한다.
-const INSERT = 'todos/INSERT'; // 새로운 todos를 등록한다.
+const INSERT = 'todos/INSERT'; // 새로운 todo를 등록한다.
 const TOGGLE = 'todos/TOGGLE'; // todo를 체크/체크 해제한다.
 const REMOVE = 'todos/REMOVE'; // todo를 제거한다.
 
-export const changeInput = input = ({
+export const changeInput = input => ({
     type: CHANGE_INPUT,
     input
 });
@@ -11,7 +11,7 @@ export const changeInput = input = ({
 let id = 3; // insert가 호출될 때마다 1씩 더해진다.
 // id 값이 3인 이유는 다음 초기 상태를 작성할 때, todo 객체 두 개를 사전에 미리 넣어 둘 것이기 때문이고,
 // 그 다음에 새로 추가될 항목의 id가 3이기 때문이다.
-export const insert = text = ({
+export const insert = text => ({
     type: INSERT,
     todo: {
         id: id++,
