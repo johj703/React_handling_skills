5f71ebd7a5d83485159dddd63729d857015f2121
johj703 <johj703@nate.com>
2022-05-29 00:40:41 +0900

    리덕스를 사용하여 리액트 애플리케이션 상태 관리하기 - 리덕스 더 편하게 사용하기 - redux-actions - counter 모듈에 적용하기

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,27 +1,22 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의 : 액션 타입은 대문자로 정의하고, 문자열 내용은 '모듈 이름/액션 이름'과 같은 형태로 작성한다.
 const INCREASE = 'Counter/INCREASE';
 const DECREASE = 'Counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
     number: 0
 };
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return {
-                number: state.number + 1
-            };
-        case DECREASE:
-            return {
-                number: state.number - 1
-            };
-        default:
-            return state;
-    }
-}
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1}),
+        [DECREASE]: (state, action) => ({ number: state.number - 1}),
+    },
+    initialState,
+);
 
 export default counter;
